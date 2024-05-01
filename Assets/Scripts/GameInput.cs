9c49f5e8a10f5ca750a3493785b6b047e100168f
BlankRip <aniketh_g5@outlook.com>
2024-05-01 13:31:55 +0530

    Key Rebinding added to options menu

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
 
 public class GameInput : MonoBehaviour
 {
@@ -10,10 +12,15 @@ public class GameInput : MonoBehaviour
     private PlayerInput playerInput;
     private void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
             playerInput = GetComponent<PlayerInput>();
+            if(PlayerPrefs.HasKey(PLAYER_PREFS_BINDINGS_KEY))
+            {
+                playerInput.actions.LoadBindingOverridesFromJson(PlayerPrefs.GetString(PLAYER_PREFS_BINDINGS_KEY));
+            }
+            Initilize();
         }
         else
         {
@@ -21,16 +28,25 @@ public class GameInput : MonoBehaviour
         }
     }
 
+    public enum Binding
+    {
+        Up, Down, Left, Right, Interact, InteractAlt, Pause
+    }
+
+    private const string PLAYER_PREFS_BINDINGS_KEY = "InputBindings"; 
+
     public event Action OnInteractAction;
     public event Action OnInteractAlternateAction;
     public event Action OnPauseAction;
 
+    private InputActionMap defaultActionMap;
     private InputAction playerMove;
     private InputAction interact;
     private InputAction interactAlternate;
     private InputAction pause;
-    private void Start()
+    private void Initilize()
     {
+        defaultActionMap = playerInput.currentActionMap;
         playerMove = playerInput.actions["Move"];
         interact = playerInput.actions["Interact"];
         interactAlternate = playerInput.actions["InteractAlternate"];
@@ -67,5 +83,75 @@ public class GameInput : MonoBehaviour
     private void PausePerformed(InputAction.CallbackContext obj)
     {
         OnPauseAction?.Invoke();
+    }
+
+    public string GetBindingText(Binding binding)
+    {
+        switch (binding)
+        {
+            case Binding.Up:
+                return playerMove.bindings[1].ToDisplayString();
+            case Binding.Down:
+                return playerMove.bindings[2].ToDisplayString();
+            case Binding.Left:
+                return playerMove.bindings[3].ToDisplayString();
+            case Binding.Right:
+                return playerMove.bindings[4].ToDisplayString();
+            case Binding.Interact:
+                return interact.bindings[0].ToDisplayString();
+            case Binding.InteractAlt:
+                return interactAlternate.bindings[0].ToDisplayString();
+            case Binding.Pause:
+                return pause.bindings[0].ToDisplayString();
+        }
+        return "N/D";
+    }
+
+    public void RebindBinding(Binding binding, Action onActionRebound)
+    {
+        defaultActionMap.Disable();
+        InputAction inputAction = playerMove;
+        int bindingIndex = 0;
+
+        switch (binding)
+        {
+            case Binding.Up:
+                inputAction = playerMove;
+                bindingIndex = 1;
+                break;
+            case Binding.Down:
+                inputAction = playerMove;
+                bindingIndex = 2;
+                break;
+            case Binding.Left:
+                inputAction = playerMove;
+                bindingIndex = 3;
+                break;
+            case Binding.Right:
+                inputAction = playerMove;
+                bindingIndex = 4;
+                break;
+            case Binding.Interact:
+                inputAction = interact;
+                bindingIndex = 0;
+                break;
+            case Binding.InteractAlt:
+                inputAction = interactAlternate;
+                bindingIndex = 0;
+                break;
+            case Binding.Pause:
+                inputAction = pause;
+                bindingIndex = 0;
+                break;
+        }
+
+        inputAction.PerformInteractiveRebinding(bindingIndex).OnComplete((callback) =>
+        {
+            callback.Dispose();
+            defaultActionMap.Enable();
+            onActionRebound?.Invoke();
+
+            PlayerPrefs.SetString(PLAYER_PREFS_BINDINGS_KEY, playerInput.actions.SaveBindingOverridesAsJson());
+        }).Start();
     }
 }
--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -6,11 +6,30 @@ using UnityEngine.UI;
 
 public class OptionsUI : MonoBehaviour
 {
+    [System.Serializable]
+    private struct KeyBindButton
+    {
+        public Button TheButton;
+        public TextMeshProUGUI TextInButton;
+    }
+
     [SerializeField] private Button soundEffectsButton;
     [SerializeField] private Button musicButton;
     [SerializeField] private Button closeButton;
     [SerializeField] private TextMeshProUGUI soundEffectsText;
     [SerializeField] private TextMeshProUGUI musicText;
+
+    [Space]
+    [Header("Key Rebind")]
+    [SerializeField] private GameObject pressToRebindInfoPanel;
+    [SerializeField] private KeyBindButton moveUpButton;
+    [SerializeField] private KeyBindButton moveDownButton;
+    [SerializeField] private KeyBindButton moveLeftButton;
+    [SerializeField] private KeyBindButton moveRightButton;
+    [SerializeField] private KeyBindButton InteractButton;
+    [SerializeField] private KeyBindButton InteractAltButton;
+    [SerializeField] private KeyBindButton pauseButton;
+
 
     private GamePauseUI gamePauseUI;
 
@@ -29,6 +48,9 @@ public class OptionsUI : MonoBehaviour
             gamePauseUI.Show();
         });
         KitchenGameManager.Instance.OnGamePauseToggled += KitchenGameManger_OnGamePauseToggled;
+
+        SetUpRebindButtons();
+        pressToRebindInfoPanel.SetActive(false);
 
         UpdateVisuals();
     }
@@ -50,6 +72,37 @@ public class OptionsUI : MonoBehaviour
     {
         soundEffectsText.SetText($"Sound Effects: {(int)(SoundManager.Instance.GetVolume() * 10)}");
         musicText.SetText($"Music: {(int)(MusicManager.Instance.GetVolume() * 10)}");
+
+        moveUpButton.TextInButton.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.Up));
+        moveDownButton.TextInButton.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.Down));
+        moveLeftButton.TextInButton.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.Left));
+        moveRightButton.TextInButton.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.Right));
+        InteractButton.TextInButton.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.Interact));
+        InteractAltButton.TextInButton.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.InteractAlt));
+        pauseButton.TextInButton.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.Pause));
+    }
+
+    private void SetUpRebindButtons()
+    {
+        moveUpButton.TheButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Up); });
+        moveDownButton.TheButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Down); });
+        moveLeftButton.TheButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Left); });
+        moveRightButton.TheButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Right); });
+        InteractButton.TheButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Interact); });
+        InteractAltButton.TheButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.InteractAlt); });
+        pauseButton.TheButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Pause); });
+    }
+
+    private void RebindBinding(GameInput.Binding binding)
+    {
+        pressToRebindInfoPanel.SetActive(true);
+        GameInput.Instance.RebindBinding(binding, OnRebindingComplete);
+    }
+
+    private void OnRebindingComplete()
+    {
+        pressToRebindInfoPanel.SetActive(false);
+        UpdateVisuals();
     }
 
     public void Show(GamePauseUI caller)
